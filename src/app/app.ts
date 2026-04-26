dafc226f813685f1cb43dec8b9c18b61341019b6
Sachini Buddhika <sachiniste3@gmail.com>
2026-04-26 17:10:08 +0530

    create the form structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,36 @@
 import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { EmployeeModel } from './model/Employee';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular_crud');
-}
+   employeeForm:FormGroup=new FormGroup({});
+   employeeObj:EmployeeModel=new EmployeeModel();
+   constructor(){
+  this.createForm()
+ }
+
+   createForm(){
+    this.employeeForm=new FormGroup({
+    empId:new FormControl(this.employeeObj.empId),
+    name:new FormControl(this.employeeObj.name),
+    city:new FormControl(this.employeeObj.city),
+    address:new FormControl(this.employeeObj.address),
+    contactNo:new FormControl(this.employeeObj.contactNo),
+    emailId:new FormControl(this.employeeObj.emailId),
+    pinCode:new FormControl(this.employeeObj.pinCode),
+    state:new FormControl(this.employeeObj.state),
+    })
+   }
+
+   onSave(){
+    
+   }
+
+  }
---
+++ src/app/model/Employee.ts
@@ -0,0 +1,11 @@
+export class EmployeeModel{
+    empId: number = 0;
+    name: string = '';
+    city: string = '';
+    state: string = '';
+    emailId: string = '';
+    contactNo: string = '';
+    address: string = '';
+    pinCode: string = '';
+
+}
